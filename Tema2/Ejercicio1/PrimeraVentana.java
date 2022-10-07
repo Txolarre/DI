f415eb705d0533270399ee91f13aa1087b11711f
Txolarre <pss120@hotmail.com>
2022-10-07 19:14:06 +0200

    crear carpetas

--- Tema2/Ejercicio1/PrimeraVentana.java
+++ Tema2/Ejercicio1/PrimeraVentana.java
@@ -1,17 +1,26 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class PrimeraVentana extends JFrame {
 
     public PrimeraVentana(){
         super("Mi primera ventana");
+
         setSize(300,100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //cambia el color de fondo a uno más chulo
         getContentPane().setBackground(Color.red);
         //coloca la ventana en el centro
         setLocationRelativeTo(null);
+        //Poner imagen de fondo
+        ImageIcon fondo=new ImageIcon("/home/pausos/IdeaProjects/DI/Tema2/Ejercicio1/donut.png");
+        JPanel panel = new JPanel();
+        //panel.add(fondo);
         setVisible(true);
 
     }
